f6d7e8c669e29a65c0cd5b1ff0fc2d5bab4ba438
morteza <mm.gov.1381@gmail.com>
2023-07-23 15:15:09 +0330

    develop getProductQuantity

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -12,6 +12,15 @@ export const cartContext = createContext({
 export function CartProvider({ children }) {
   const [cartProducts, setCartProducts] = useState([]);
 
+  function getProductQuantity(id) {
+    const quantity = cartProducts.find((item) => item.id === id)?.quantity;
+
+    if (quantity === undefined) {
+      return 0;
+    }
+    return quantity;
+  }
+
   const ContextValue = {
     items: cartProducts,
     getProductQuantity,
