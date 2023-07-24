aeb0708fb982fb393291f7cd695bb986272d5664
morteza <mm.gov.1381@gmail.com>
2023-07-24 12:28:02 +0330

    add and delete item button

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,10 +1,12 @@
 import { useContext } from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button, Form, Row, Col } from "react-bootstrap";
 
 import { CartContext } from "../context/CartContext";
 
 function ProductItem({ product }) {
   const cart = useContext(CartContext);
+
+  const productQuantity = cart.getProductQuantity(product.id);
 
   return (
     <Card className="mt-5 card-bg">
@@ -18,16 +20,44 @@ function ProductItem({ product }) {
         <Card.Title align="right" className="text-light pt-4">
           {product.title}
         </Card.Title>
-        <Card.Text align="right" className="text-light">
-          {product.price}
+        <Card.Text align="right" className="text-light" dir="rtl">
+          {product.price} تومان
         </Card.Text>
-        <Button
-          onClick={() => cart.addItemToCart(product.id)}
-          variant="btn btn-outline-secondary"
-          className="text-white"
-        >
-          افزودن به سبد خرید
-        </Button>
+        {productQuantity > 0 ? (
+          <>
+            <Form as={Row}>
+              <Form.Label column="true" sm="6" className="text-white">
+                تعداد:{productQuantity}
+              </Form.Label>
+              <Col sm="6">
+                <Button
+                  onClick={() => cart.addItemToCart(product.id)}
+                  sm="6"
+                  className="mx-2 text-white"
+                  variant="btn btn-outline-secondary"
+                >
+                  +
+                </Button>
+                <Button
+                  onClick={() => cart.removeItemFromCart(product.id)}
+                  sm="6"
+                  className="mx-2 text-white"
+                  variant="btn btn-outline-secondary"
+                >
+                  -
+                </Button>
+              </Col>
+            </Form>
+          </>
+        ) : (
+          <Button
+            onClick={() => cart.addItemToCart(product.id)}
+            variant="btn btn-outline-secondary"
+            className="text-white"
+          >
+            افزودن به سبد خرید
+          </Button>
+        )}
       </Card.Body>
     </Card>
   );
