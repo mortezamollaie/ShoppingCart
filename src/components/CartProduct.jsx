6b15d0832c54f88f80ded3a8fbf6033c9d252a72
morteza <mm.gov.1381@gmail.com>
2023-07-24 13:25:11 +0330

    update

---
+++ src/components/CartProduct.jsx
@@ -0,0 +1,29 @@
+import { useContext } from "react";
+
+import { Button } from "react-bootstrap";
+
+import { CartContext } from "../context/CartContext";
+
+import { getProductData } from "../data/items";
+
+function CartProduct({ id, quantity }) {
+  const cart = useContext(CartContext);
+  const productData = getProductData(id);
+  return (
+    <>
+      <p>{productData.title}</p>
+      <p>تعداد:{quantity}</p>
+      <p>قیمت:{productData.price * quantity}</p>
+      <Button
+        size="sm"
+        className="mb-5 text-white"
+        variant="btn btn-outline-secondary"
+        onClick={() => cart.deleteFromCart(id)}
+      >
+        حذف
+      </Button>
+    </>
+  );
+}
+
+export default CartProduct;
